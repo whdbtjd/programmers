fd2ceeb646ae1caf9b9e17362ede4afb57c64cc6
whdbtjd <cys990617@gmail.com>
2024-08-25 14:30:57 +0900

    기능 개발

---
+++ pro16/pro16.java
@@ -0,0 +1,39 @@
+package problem;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// 기능 개발
+public class pro16 {
+	public static void main(String args[]) {
+		System.out.println(Arrays.toString(solution(new int[] { 93, 30, 55 }, new int[] { 1, 30, 5 })));
+	}
+
+	static int[] solution(int progresses[], int speeds[]) {
+
+		int day = 0;
+		ArrayDeque<Integer> queue = new ArrayDeque<>();
+
+		ArrayList<Integer> list = new ArrayList<>(); // 정답을 저장해둘 배열
+
+		for (int i = 0; i < progresses.length; i++) {
+			// 해당 기능이 개발되는데 걸리는 시간 계산
+			day = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
+
+			if (!queue.isEmpty() && queue.peekLast() < day) {
+				list.add(queue.size());
+				queue.clear();
+			}
+				queue.add(day);
+			
+		
+		}
+
+		// 큐에 남은 원소
+		list.add(queue.size());
+
+		return list.stream().mapToInt(x -> x.intValue()).toArray();
+	}
+
+}
